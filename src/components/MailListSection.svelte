eb9e4eb7715e5b796428b47455136aa04a540b25
twinstae <rabolution@gmail.com>
2021-03-17 03:25:24 +0900

    리스트 디버깅, 검색 추가

--- src/components/MailListSection.svelte
+++ src/components/MailListSection.svelte
@@ -1,125 +1,141 @@
 <script lang="ts">
-    import BottomPagenation from './BottomPagenation.svelte';
-    import ListItem from "./MailListItem.svelte";
-    import MailCardItem from './MailCardItem.svelte';
-    import { afterUpdate } from "svelte";
-    import { dateString, date_to_str, str_to_date, time_to_dateStr } from "../stores/date";
-    import {selected_tag, tag_to_mail_dict} from "../stores/tag";
-    import pm_list from "../pages/door/_pm_list.json";
-    import { now_pm, now_page } from '../stores/now';
+import BottomPagenation from './BottomPagenation.svelte';
+import ListItem from "./MailListItem.svelte";
+import MailCardItem from './MailCardItem.svelte';
+import { afterUpdate } from "svelte";
+import { dateString, date_to_str, str_to_date, time_to_dateStr } from "../stores/date";
+import {selected_tag, tag_to_mail_dict} from "../stores/tag";
+import pm_list from "../pages/door/_pm_list.json";
+import { now_pm, now_page } from '../stores/now';
 
-    const onMailSelected = (pm)=> ()=>{
-        if(pm){$now_pm=pm}
+import Fuse from 'fuse.js'
+const options = {
+    keys: [
+        "subject",
+        "preview",
+    ]
+};
+
+const fuse = new Fuse(pm_list, options);
+let search_input ="";
+
+$: pm_list_after_search = search_input
+    ? fuse.search(search_input).map(result=>result.item) 
+    : pm_list;
+
+const onMailSelected = (pm)=> ()=>{
+    if(pm){$now_pm=pm}
+}
+
+let lastDateString;
+$: now_date = str_to_date($dateString);
+
+let lastNowPage=1;
+let lastMailPerPage=3;
+let anchor_mail;
+    
+afterUpdate(() => {
+    if (maxPage < $now_page){
+        $now_page=maxPage;
+        return null;
+    }
+    if (0 >= $now_page || $now_page == null){
+        $now_page=1;
+        return null;
     }
 
-    let lastDateString;
-    $: now_date = str_to_date($dateString);
+    const first_mail = mail_list[0];
+    if (lastNowPage != $now_page && first_mail){
+        anchor_mail = first_mail;
+        lastNowPage = $now_page;
+    }
 
-    let lastNowPage=1;
-    let lastMailPerPage=3;
-    let anchor_mail;
-        
-    afterUpdate(() => {
-        if (maxPage < $now_page){
-            $now_page=maxPage;
-            return null;
-        }
-        if (0 >= $now_page){
-            $now_page=1;
-            return null;
-        }
-
-        const first_mail = mail_list[0];
-        if (lastNowPage != $now_page && first_mail.id){
-            anchor_mail = first_mail;
-            lastNowPage = $now_page;
-        }
-
-        if (lastMailPerPage != mail_per_page){
-            let first_mail_index;
-            filtered_list.forEach((mail,i)=>{
-                if (first_mail_index==null && mail==anchor_mail){
-                    first_mail_index = i;
-                }
-            })
-            $now_page=Math.ceil((first_mail_index+1) / mail_per_page);
-            lastNowPage=$now_page;
-            lastMailPerPage = mail_per_page;
-            return null;
-        }
-
-        if (now_date > new Date()){
-            $dateString = date_to_str(new Date());
-            alert("미래로 갈 수는 없습니다.")
-            return null;
-        }
-
-        if (!$selected_tag.value || mail_list.length==0) {
-            return null
-        }
-
-        if (lastDateString != $dateString){
-
-            let result = false;
-            filtered_list.forEach((mail, i)=>{
-                if (result){
-                    return null;
-                }
-                const mail_date_str = time_to_dateStr(mail.time);
-                const mail_date = str_to_date(mail_date_str);
-                if (mail_date <= now_date){
-                    $now_page = Math.ceil((i+1) / mail_per_page);
-                    $dateString = mail_date_str;
-                    result = true;
-                }
-            })
-
-            if (result==false){
-                $now_page = maxPage;
-                const last_mail = filtered_list[filtered_list.length-1];
-                $dateString = time_to_dateStr(last_mail.time);
+    if (lastMailPerPage != mail_per_page && anchor_mail){
+        let first_mail_index;
+        filtered_list.forEach((mail,i)=>{
+            if (first_mail_index==null && mail==anchor_mail){
+                first_mail_index = i;
             }
-            lastDateString = $dateString;
-            return null;
-        }
-        const first_date_str = time_to_dateStr(mail_list[0].time);
-        $dateString = first_date_str;
-        lastDateString = first_date_str;
-    });
-
-    $: selected_tag_mail_set = $tag_to_mail_dict.get($selected_tag);
-    
-    $: filterByTag = (mail)=> selected_tag_mail_set.has(mail.id);
-
-    const filterByDate = mail => mail.time.split(" ")[0].replaceAll("/", "-") == $dateString;
-
-    const no_filter = (mail)=>true;
-
-    $: filter_by = 
-        ($selected_tag.value && filterByTag) ||
-        ($dateString && filterByDate) || 
-        no_filter;
-    $: filtered_list = pm_list.filter(filter_by);
-
-    let mail_list_width;
-    let mail_list_height;
-    $: mail_per_width =  Math.floor((mail_list_width) / 280)
-    $: mail_per_height = Math.floor(mail_list_height * 5/6 / 200);
-    $: mail_per_page = isListView ? 10 : mail_per_width * mail_per_height || 6;
-
-    $: maxPage = Math.ceil(filtered_list.length/mail_per_page);
-    
-    $: pagination = ($now_page-1) * mail_per_page;
-    $: getPage = ()=>{
-        const page = filtered_list.slice(pagination, pagination + mail_per_page)
-        if(page.length < mail_per_page){
-            return page.concat(Array(mail_per_page-page.length).fill({"id": "", "member": "", "time": "", "subject": "", "preview": ""}))
-        }
-        return page;
+        })
+        $now_page=Math.ceil((first_mail_index+1) / mail_per_page);
+        lastNowPage=$now_page;
+        lastMailPerPage = mail_per_page;
+        return null;
     }
-    $: mail_list = filtered_list && getPage();
 
-    let isListView = false;
+    if (now_date > new Date()){
+        $dateString = date_to_str(new Date());
+        alert("미래로 갈 수는 없습니다.")
+        return null;
+    }
+
+    if (!$selected_tag.value || mail_list.length==0) {
+        return null
+    }
+
+    if (lastDateString != $dateString){
+
+        let result = false;
+        filtered_list.forEach((mail, i)=>{
+            if (result){
+                return null;
+            }
+            const mail_date_str = time_to_dateStr(mail.time);
+            const mail_date = str_to_date(mail_date_str);
+            if (mail_date <= now_date){
+                $now_page = Math.ceil((i+1) / mail_per_page);
+                $dateString = mail_date_str;
+                result = true;
+            }
+        })
+
+        if (result==false){
+            $now_page = maxPage;
+            const last_mail = filtered_list[filtered_list.length-1];
+            $dateString = time_to_dateStr(last_mail.time);
+        }
+        lastDateString = $dateString;
+        return null;
+    }
+    const first_date_str = time_to_dateStr(mail_list[0].time);
+    $dateString = first_date_str;
+    lastDateString = first_date_str;
+});
+
+$: selected_tag_mail_set = $tag_to_mail_dict.get($selected_tag);
+
+$: filterByTag = (mail)=> selected_tag_mail_set.has(mail.id);
+
+const filterByDate = mail => mail.time.split(" ")[0].replaceAll("/", "-") == $dateString;
+
+const no_filter = (mail)=>true;
+
+$: filter_by = 
+    ($selected_tag.value && filterByTag) ||
+    (search_input && no_filter)||
+    ($dateString && filterByDate) || 
+    no_filter;
+$: filtered_list = pm_list_after_search.filter(filter_by);
+
+let mail_list_width;
+let mail_list_height;
+$: mail_per_width =  Math.floor((mail_list_width) / 280)
+$: mail_per_height = Math.floor(mail_list_height * 5/6 / 200);
+$: mail_per_page = isListView ? 10 : mail_per_width * mail_per_height || 6;
+
+$: maxPage = Math.ceil(filtered_list.length/mail_per_page);
+
+$: pagination = ($now_page-1) * mail_per_page;
+$: getPage = ()=>{
+    const page = filtered_list.slice(pagination, pagination + mail_per_page)
+    if(page.length < mail_per_page){
+        return page.concat(Array(mail_per_page-page.length).fill({"id": "", "member": "", "time": "", "subject": "", "preview": ""}))
+    }
+    return page;
+}
+$: mail_list = filtered_list && getPage();
+
+let isListView = false;
 
 </script>
 
@@ -149,7 +165,7 @@ class="h-5/6 relative m-5">
             {/each}
         </ul>
     {/if}
-    
-    <BottomPagenation
-    maxPage={maxPage}/>
+    <BottomPagenation maxPage={maxPage}/>
+    <input class="m-2"
+    type="text" bind:value={search_input} placeholder="검색"/>
 </section>
