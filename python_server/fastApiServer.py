b1cf4f774d590f6a9dae93f1759dc7c1395e8467
twinstae <rabolution@gmail.com>
2021-03-19 12:11:47 +0900

    fast_server

--- python_server/fastApiServer.py
+++ python_server/fastApiServer.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
 from all_tag_dict import router as all_tag_router
 # from mail_to_tag_dict import router as mail_to_tag_dict_router
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return FileResponse('output/index.html')
+    
+app.mount("/", StaticFiles(directory="output"), name="static")
 app.include_router(all_tag_router)
 # app.include_router(mail_to_tag_dict_router)
