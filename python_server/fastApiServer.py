ccf8a51d247869d8e794473ce58787a94f59c850
twinstae <rabolution@gmail.com>
2021-03-25 18:01:02 +0900

    자동 서버 업데이트 (자기 자신을...)

--- python_server/fastApiServer.py
+++ python_server/fastApiServer.py
@@ -4,7 +4,6 @@ import shutil
 import time
 import json
 import os
-import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -12,6 +11,7 @@ from starlette.responses import FileResponse
 import httpx
 
 from ApiRouter import api_router
+
 
 app = FastAPI()
 
@@ -28,7 +28,7 @@ app.add_middleware(
 )
 
 client = httpx.AsyncClient()
-GITHUB_URL = "https://raw.githubusercontent.com/twinstae/izone-pm-viewer/main/dist"
+GITHUB_URL = "https://raw.githubusercontent.com/twinstae/izone-pm-viewer/main"
 
 
 @app.on_event("startup")
@@ -37,67 +37,34 @@ async def startup_event():
     start = time.time()
 
     try:
-        build_url = await get_build_url()
+        build_url, server_source_url = await get_build_url()
         build_info = await get_json(build_url)
+        server_info = await get_json(server_source_url)
 
         file_list = [path_obj["path"] for path_obj in build_info["tree"]]
-        print(file_list)
         if not await download_latest_build(file_list):
             print("이미 최신 빌드입니다.")
+
+        server_source_list = [path_obj["path"] for path_obj in server_info["tree"]]
+        if not await download_latest_server(server_source_list):
+            print("이미 최신 서버입니다")
+
         duration = time.time() - start
-        print("소요시간 %s 초, 빌드 파일을 최신화했습니다." % str(int(duration * 100) / 100))
+        print("소요시간 %s 초, 최신 버전입니다." % str(int(duration * 100) / 100))
     except Exception as e:
         print("최신 빌드 로딩에 실패했습니다.")
         print(e)
 
 
-async def download_latest_build(file_list: List[str]) -> bool:
-    if os.path.exists("output/build") and set(file_list) == set(os.listdir("output/build")):
-        return False
-
-    if os.path.exists("output/build"):
-        move_builds_to_old_dir()
-        os.rmdir("output/build")
-
-    os.mkdir("output/build")
-    path_list = ["/index.html"] + ["/build/" + file_name for file_name in file_list]
-    await asyncio.wait([get_and_write_file(path) for path in path_list])
-    return True
-
-
 async def get_build_url():
     main_data = await get_json("https://api.github.com/repos/twinstae/izone-pm-viewer/git/trees/main")
     dist_url = find_url_for_path_from_tree(main_data, "dist")
+    server_source_url = find_url_for_path_from_tree(main_data, "python_server")
+
     dist_data = await get_json(dist_url)
+
     build_url = find_url_for_path_from_tree(dist_data, "build")
-    return build_url
-
-
-def move_builds_to_old_dir() -> None:
-    if not os.path.exists("output/old"):
-        os.mkdir("output/old")
-
-    if os.path.exists("output/old/build"):
-        shutil.rmtree("output/old/build")
-
-    os.mkdir("output/old/build")
-    for file_name in os.listdir("output/build"):
-        shutil.move(f"output/build/{file_name}", f"output/old/build/{file_name}")
-
-
-def find_url_for_path_from_tree(data, path):
-    for obj in data["tree"]:
-        if obj["path"] == path:
-            return obj["url"]
-
-
-async def get_and_write_file(file_name):
-    file_path = GITHUB_URL + file_name
-    content = await get_content(file_path)
-    write_file(
-        file_path='output' + file_name,
-        content=content
-    )
+    return build_url, server_source_url
 
 
 async def get_content(url: str):
@@ -108,6 +75,65 @@ async def get_content(url: str):
 async def get_json(url: str):
     json_data = await get_content(url)
     return json.loads(json_data)
+
+
+def move_to_old_dir(dir_path) -> None:
+    if not os.path.exists("output/old"):
+        os.mkdir("output/old")
+
+    if os.path.exists("output/old"+dir_path):
+        shutil.rmtree("output/old"+dir_path)
+
+    os.mkdir("output/old"+dir_path)
+    for file_name in os.listdir("output"+dir_path):
+        shutil.move(f"output{dir_path}/{file_name}", f"output/old{dir_path}/{file_name}")
+
+
+def find_url_for_path_from_tree(data, path):
+    for obj in data["tree"]:
+        if obj["path"] == path:
+            return obj["url"]
+
+
+PYTHON_SERVER_DIR = "output/python_server"
+
+
+async def download_latest_server(source_list: List[str]) -> bool:
+    if os.path.exists(PYTHON_SERVER_DIR) and set(source_list) == set(os.listdir(PYTHON_SERVER_DIR)):
+        return False
+
+    if os.path.exists(PYTHON_SERVER_DIR):
+        move_to_old_dir("/python_server")
+        os.rmdir(PYTHON_SERVER_DIR)
+
+    os.mkdir(PYTHON_SERVER_DIR)
+    path_list = ["/python_server/" + source_name for source_name in source_list]
+    await asyncio.wait([get_and_write_file(path, "") for path in path_list])
+    return True
+
+
+async def download_latest_build(file_list: List[str]) -> bool:
+    if os.path.exists("output/build") and set(file_list) == set(os.listdir("output/build")):
+        return False
+
+    if os.path.exists("output/build"):
+        move_to_old_dir("/build")
+        os.rmdir("output/build")
+
+    os.mkdir("output/build")
+    path_list = ["/index.html"] + ["/build/" + file_name for file_name in file_list]
+    await asyncio.wait([get_and_write_file(path, "/dist") for path in path_list])
+    return True
+
+
+async def get_and_write_file(file_name, prefix):
+    content = await get_content(GITHUB_URL + prefix + file_name)
+    file_path = 'output' + file_name
+    write_file(
+        file_path=file_path,
+        content=content
+    )
+    print(file_path)
 
 
 def write_file(file_path: str, content: str):
