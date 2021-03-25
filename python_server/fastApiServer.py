205385cb6a9738f64e8d8700f168ef1c4640a0ff
twinstae <rabolution@gmail.com>
2021-03-25 18:50:30 +0900

    종료 시 서버 업데이트 코드

--- python_server/fastApiServer.py
+++ python_server/fastApiServer.py
@@ -1,9 +1,9 @@
 from typing import List
 import asyncio
-import shutil
 import time
 import json
 import os
+import shutil
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -48,16 +48,29 @@ async def startup_event():
         server_source_list = [path_obj["path"] for path_obj in server_info["tree"]]
         if not await download_latest_server(server_source_list):
             print("이미 최신 서버입니다")
+        else:
+            print("최신 서버 파일을 불러왔습니다. 서버를 끌 때 업데이트 됩니다.")
 
         duration = time.time() - start
+
         print("소요시간 %s 초, 최신 버전입니다." % str(int(duration * 100) / 100))
     except Exception as e:
         print("최신 빌드 로딩에 실패했습니다.")
         print(e)
 
 
+class 깃허브_서버_허용량_초과(Exception):
+    message = "시간 당 깃허브 API 요청 가능 횟수를 넘었습니다. 1시간 뒤부터 다시 서버 업데이트가 가능합니다."
+
+
 async def get_build_url():
     main_data = await get_json("https://api.github.com/repos/twinstae/izone-pm-viewer/git/trees/main")
+    
+    if "tree" not in main_data and "message" in main_data:
+        print(main_data)
+        raise 깃허브_서버_허용량_초과()
+        
+    
     dist_url = find_url_for_path_from_tree(main_data, "dist")
     server_source_url = find_url_for_path_from_tree(main_data, "python_server")
 
@@ -99,7 +112,12 @@ PYTHON_SERVER_DIR = "output/python_server"
 
 
 async def download_latest_server(source_list: List[str]) -> bool:
-    if os.path.exists(PYTHON_SERVER_DIR) and set(source_list) == set(os.listdir(PYTHON_SERVER_DIR)):
+    file_list = os.listdir("output/python_server")
+    old_server_version = get_server_version(file_list)
+    new_server_version = get_server_version(source_list)
+    print("old: ", ".".join(old_server_version))
+    print("new: ", ".".join(new_server_version))
+    if 이미_최신버전인가(old_server_version, new_server_version):
         return False
 
     if os.path.exists(PYTHON_SERVER_DIR):
@@ -144,7 +162,48 @@ def write_file(file_path: str, content: str):
 @app.on_event("shutdown")
 async def shutdown_event():
     await client.aclose()
+    print("최신 서버 파일을 적용합니다.")
+    try:
+        현재_서버_파일을_지우고_새_서버를_가져온다()
+    except Exception as e:
+        print("서버 업데이트에 실패했습니다.", e)
 
+
+def 현재_서버_파일을_지우고_새_서버를_가져온다() -> bool:
+    if not os.path.exists("output/python_server"):
+        return False
+    old_server_version = get_server_version(os.listdir("."))
+
+    file_list = os.listdir("output/python_server")
+    new_server_version = get_server_version(file_list)
+
+    if 이미_최신버전인가(old_server_version, new_server_version):
+        print("서버가 이미 최신 버전입니다.")
+        return True
+
+    for file_name in file_list:
+        old_path = "./"+file_name
+        os.remove(old_path)
+        assert not os.path.exists(old_path)
+        new_file_src = "output/python_server/"+file_name
+        shutil.copy(new_file_src, old_path)
+        assert os.path.exists(old_path)
+        print(old_path)
+    print("최신 서버 파일을 적용했습니다.")
+    return True
+
+
+def get_server_version(file_list):
+    for file_name in file_list:
+        if "버전" in file_name:
+            return file_name.replace("버전", "").split(".")
+
+
+def 이미_최신버전인가(old_ver, new_ver):
+    for old_n, new_n in zip(old_ver, new_ver):
+        if old_n != new_n:
+            return old_n >= new_n
+    return True
 
 @app.get("/")
 async def root():
