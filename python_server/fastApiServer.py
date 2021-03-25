496eb39fd6789555d46273b8a94f1b5df45a1729
twinstae <rabolution@gmail.com>
2021-03-25 12:39:58 +0900

    최신 빌드 로딩 로직 개선

--- python_server/fastApiServer.py
+++ python_server/fastApiServer.py
@@ -1,3 +1,8 @@
+import asyncio
+import shutil
+import time
+
+import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -29,22 +34,65 @@ GITHUB_URL = "https://raw.githubusercontent.com/twinstae/izone-pm-viewer/main/di
 
 @app.on_event("startup")
 async def startup_event():
-    tree_info = await get_json("https://api.github.com/repos/twinstae/izone-pm-viewer/git/trees/main")
-    dist_url = get_dist_url(tree_info["tree"])
-    
+    print("최신 빌드를 다운로드합니다.")
+    start = time.time()
 
-    "https://api.github.com/repos/twinstae/izone-pm-viewer/git/trees/742961d953ad96a4b30bffcf84b841acb29b5d72"
-    content = await get_content(GITHUB_URL+"/build/"+file_name)
+    try:
+        build_url = await get_build_url()
+        build_info = await get_json(build_url)
+        file_list = ["/index.html"] + [
+                "/build/" + path_obj["path"]
+                for path_obj in build_info["tree"]
+            ]
+        print(file_list)
+
+        if os.path.exists("output/build"):
+            move_builds_to_old_dir()
+            os.rmdir("output/build")
+
+        os.mkdir("output/build")
+        await asyncio.wait([get_and_write_file(file_name) for file_name in file_list])
+
+        duration = time.time() - start
+        print("소요시간 %s 초, 빌드 파일을 최신화했습니다." % str(int(duration * 100) / 100))
+    except Exception as e:
+        print("최신 빌드 로딩에 실패했습니다.")
+        print(e)
+
+
+async def get_build_url():
+    main_data = await get_json("https://api.github.com/repos/twinstae/izone-pm-viewer/git/trees/main")
+    dist_url = find_url_for_path_from_tree(main_data, "dist")
+    dist_data = await get_json(dist_url)
+    build_url = find_url_for_path_from_tree(dist_data, "build")
+    return build_url
+
+
+def move_builds_to_old_dir() -> None:
+    if not os.path.exists("output/old"):
+        os.mkdir("output/old")
+
+    if os.path.exists("output/old/build"):
+        shutil.rmtree("output/old/build")
+
+    os.mkdir("output/old/build")
+    for file_name in os.listdir("output/build"):
+        shutil.move(f"output/build/{file_name}", f"output/old/build/{file_name}")
+
+
+def find_url_for_path_from_tree(data, path):
+    for obj in data["tree"]:
+        if obj["path"] == path:
+            return obj["url"]
+
+
+async def get_and_write_file(file_name):
+    file_path = GITHUB_URL + file_name
+    content = await get_content(file_path)
     write_file(
-        file_path='output/offline/build/' + file_name,
+        file_path='output' + file_name,
         content=content
     )
-
-
-def get_dist_url(path_list):
-    for path_obj in path_list:
-        if path_obj["path"] == "dist":
-            return path_obj["url"]
 
 
 @app.on_event("shutdown")
@@ -76,3 +124,7 @@ def write_file(file_path: str, content: str):
 
 app.include_router(api_router)
 app.mount("/", StaticFiles(directory="output"), name="static")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
